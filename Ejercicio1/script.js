c241bce877e596c33871500165e4836d311919e2
Aitor <73170304+AitorEnriquez@users.noreply.github.com>
2021-11-29 20:55:09 +0100

    mis cambios de casa

--- Ejercicio1/script.js
+++ Ejercicio1/script.js
@@ -10,7 +10,7 @@ $(function() {
         //Las injurias se eliminarán
         $("p").hide();
         //La sociedad te mostrará su agradecimiento
-        $("p").last().after("<h2>¡Enhorabuena! Has eliminado todas las injurias del mundo. Ahora la sociedad está agradecida contigo.</h2>");
+        $("input").before("<h2>¡Enhorabuena! Has eliminado todas las injurias del mundo. Ahora la sociedad está agradecida contigo.</h2>");
         $("input").val("Gracias");
         //Ya no podrás pulsar el botón, porque no tendría ningún efecto...
         //Aunque ahora que pienso quizá se podría aplicar el efecto contrario y te convertirías en un villano al que la sociedad repugna pero me da pereza hacerlo
@@ -49,26 +49,35 @@ $(function() {
     $("img").click(function(){
         //Se irá corriendo a denunciarte
         $("img").css("position","relative");
-        $("img").animate({left: "-=" + (600*contador)},300*contador,"swing", function () {
+        $("img").animate({left: "-=" + (2000*contador)},600*contador,"swing", function () {
              (this.remove());
         });
         $(".aviso").remove();
         //La página te avisa de la fechoría que acabas de cometer
-        $("input").last().after("<h2>¡NOOOOOOO! ¡Has enfadado a la gallina y ahora ha ido a denunciarte!</h2>")
+        $("input").last().after("<h2>¡NOOOOOOO! ¡Has enfadado a la gallina y ahora ha ido a denunciarte!</h2>");
+    
+        //Fución en 5 segundos
+        var intervalo = setInterval(function() {
+            //Ya has delinquido, no pasa por un poco más; se te muestra una opción para fugarte
+                $("h2").last().after("<h2>¡Será mejor que vayas a esconderte por ahí, antes de que te pille la policía!</h2>");
+                $("h2").last().after("<a href=../Ejercicio7,8,9/index.html>Darse a la fuga</a>");
+                clearInterval(intervalo);
+            },5000)
+    
     })
 
 
     //HUEVO DE PASCUA!!
     document.addEventListener("keyup",function () {
         //Si pulso una tecla del teclado la gallina pondrá un pollito (o más, depende que se escriba aquí abajo)
-        for (let i =0; i<100;i++) {
+       let gallinasPorTecla = 100;
+        for (let i =0; i<gallinasPorTecla;i++) {
             $("img").last().after(pollito); 
             //Contador de pollitos 
             $('.aviso+p').remove();
             $(".aviso").last().after("<p>¡Has generado un pollito!(" +contador + ")</p>");
             contador++;
         }
-        // $("img").last().css("width","100px")
     })
 
 });
--- Ejercicio7,8,9/script.js
+++ Ejercicio7,8,9/script.js
@@ -1,5 +1,6 @@
 //Popurrí de variables
 primerAviso = false;
+salidaCueva = false;
 
 //Cuando se carga el documento
 $(function() {
@@ -18,14 +19,33 @@ $(function() {
 
         $('#entradaCueva').dblclick(function(){
             $('#entradaCueva').animate({width: 100})
-            window.open('cueva.html','blank');
+            window.open('cueva.html');
             window.close();
         });
       
         $('#salirDerrumbe').click(function() {
-            $('#derrumbre').slideDown();
+            $('#derrumbe,#derrumbeFondo').animate({bottom:0,top:0},1000);
+            $('#derrumbe').animate({opacity:1},1000);
         });
       
+        $('#salirCueva').click(function(){
+            salidaCueva = true;
+            localStorage.setItem("salidaCueva",salidaCueva);
+            window.open('index.html');
+            window.close();
+        })
+
+        if (localStorage.getItem("salidaCueva")=="true") {
+            document.getElementById("h1").innerHTML = "¡Qué bien que has podido salir de la cueva!";
+        }
+        
+       var intervalo =  setInterval(function() {
+            salidaCueva = false;
+            localStorage.setItem("salidaCueva",salidaCueva);
+            document.getElementById("h1").innerHTML = "¡Has encontrado una cueva! Puedes explorarla si lo deseas";
+            clearInterval(intervalo);
+        },5000);
+
     });
 
 
